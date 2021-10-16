21d1dc9e662e85bba3b8255a4e5c83a659c7758b
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-10-16 21:48:38 +0530

    create new files for learning mongodb

---
+++ mongo-demo/index.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+.then(() => console.log("Connected to the MongoDB..."))
+.catch(err => console.error("Could not connected to the MongoDB...", err));
+
+const courseschema = new mongoose.Schema ({
+    name: String,
+    author: String,
+    tags:[String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+})
+
+const Course = mongoose.model('Course', courseschema);
+
+async function createCourse() {
+    
+    const course = new Course ({
+        name: "Vue course",
+        author: "Gayathri",
+        tags: ["Vue", "Frontend"],
+        isPublished: true
+    })
+
+    const result = await course.save();
+    console.log(result);
+}
+
+async function getCourse() {
+    const courses = await Course
+    .find({author: "Gayathri"})
+    .limit(10)
+    .sort({name: 1})
+    .select({name: 1, tags:1});
+    console.log(courses);
+}
+
+// createCourse();
+getCourse();
+
